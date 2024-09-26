4124b1d237786d4513fe410a67ca3ec311202361
iamapen <iamapen@studiopoppy.com>
2024-09-27 00:15:11 +0900

    phpcsルール調整

--- src/Sample/Foo.php
+++ src/Sample/Foo.php
@@ -3,7 +3,22 @@ declare(strict_types=1);
 
 namespace Acme\Sample;
 
+/**
+ * Foo
+ */
 class Foo
 {
+    // a
 
+    // b
+
+
+    public function foo()
+    {
+    }
+
+    public function bar()
+    {
+        var_dump('hoge');
+    }
 }
